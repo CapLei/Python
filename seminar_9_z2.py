6cc3a5b31b943317b7c6612dd24609663a09210f
Vlad Dre <dreier.vlad@mail.ru>
2024-10-21 22:59:16 +0300

    Задача с файлами csv

---
+++ seminar_9_z2.py
@@ -0,0 +1,10 @@
+from pandas import read_csv
+
+
+adv1_df = pd.read_csv('advertising_1.csv', index_col='Number')
+adv1_df.head(10)
+
+adv1_df.loc[8, 'Daily Internet Usage']
+
+adv2_df = pd.read_csv('advertising_2.csv', index_col='Number')
+adv2_df.loc[533:536]
